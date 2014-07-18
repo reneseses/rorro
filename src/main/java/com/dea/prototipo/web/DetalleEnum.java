17ed6f65e1fbd2d62a9f6e228f5b4ea0b44cb38e
loosseerr <r.araya.villalobos@gmail.com>
2014-07-17 23:06:32 -0400

    Ya ahí están los restantes :3

--- src/main/java/com/dea/prototipo/web/DetalleEnum.java
+++ src/main/java/com/dea/prototipo/web/DetalleEnum.java
@@ -15,7 +15,20 @@ MaquinaRecogedoraGuiadaPorCable(35000d),
 VehiculoGuiadoAutomaticamente(80000d),
 Carretilla(644d),
 TranspaletaManual(3000d),
-MontaCargasHibrido(100000d);
+MontaCargasHibrido(100000d),
+PersonaABordoDeLaMaquinaAutomatizadaDeAlmacenamientoYRecogida(110000d),
+CarruselHorizontal(40000d),
+CarruselVertical(65000d),
+MaquinaAutomatizadaDeAlmacenamientoYRecogidaParaMinicargas(125000d),
+ModuloDispensadorAFrame(750d),
+TransportadorDeRodillo(50d),
+TransportadorDeRodilloAccionadoOMotorizado(200d),
+TransportadorDeCinta(100d),
+TransportadorDeRuedas(25d),
+TransportadorDeLineaDeRemolque(100d),
+ClasificadorBandejaMovible(1000d),
+TransportadorDePallet(1000d);
+
     
     private double detalle;
 	private DetalleEnum(double detalle){
